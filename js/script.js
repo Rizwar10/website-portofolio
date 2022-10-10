bf52ad0234748d9aa72527fbba4d32929b7e4735
Rizwarnugroho <rizwarnugroho@gmail.com>
2022-10-10 15:01:58 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+let social = document.querySelector('#social');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('active');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '45px',
+	duration: 2700,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:350, origin:'left'})
+sr.reveal('.foto-saya',{delay:350, origin:'right'})
+
+sr.reveal('.social,.contact',{delay:200, origin:'bottom'})
+
+
